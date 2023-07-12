91487c484dbe72769781641298bfeb5f61308f6e
DJEnotJungle <danchiknik2000@gmail.com>
2023-07-12 11:49:01 +0300

    No mocks, only methods

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,42 +1,27 @@
 package com.example;
 
 import org.hamcrest.MatcherAssert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
-    @Mock
-    private Feline feline;
-
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
+    Feline feline = new Feline();
     @Test
     public void hunterFeline() throws Exception {
-        List<String> actualFood = (new Feline()).eatMeat();
+        List<String> actualFood = feline.eatMeat();
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
         MatcherAssert.assertThat("Вот что я люблю", expectedFood, is(actualFood));
     }
     @Test
     public void isItACat(){
         String expectedFamily = "Кошачьи";
-        when(feline.getFamily()).thenReturn(expectedFamily);
         String actualFamily = feline.getFamily();
         MatcherAssert.assertThat("Я не котик", expectedFamily, is(actualFamily));
     }
     @Test
     public void howManyKittens(){
         int kittensCount = 1;
-        when(feline.getKittens()).thenReturn(kittensCount);
         int actualKittens = feline.getKittens();
         MatcherAssert.assertThat("Где котята Мурковски", kittensCount, is(actualKittens));
     }
